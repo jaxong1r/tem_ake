611e2f4d690f5876d43d69df03eb0042a218c110
jaxong1r <jahongirmumbekov@gmail.com>
2024-09-14 01:32:49 +0500

    Update Works.jsx

--- src/Works.jsx
+++ src/Works.jsx
@@ -6,7 +6,7 @@ function Works({img,h4,yil,p}) {
   return (
     
     <div className='work_wrapper'>
-       <img width={200} src={img} alt="" /> 
+       <img width={300} src={img} alt="" /> 
       <div>
         <h4>{h4}</h4>
         <div className="span_w">
